76a8b1c1eb499f5d94aa2eb9ac7fdab560ea4e15
xennia316 <xennia316@gmail.com>
2021-08-09 19:02:35 +0100

    work in progress

---
+++ ch02/dweight.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void){
+    int height, lenght, width, volume,  weight;
+
+    printf("Enter height of box: ");
+    scanf("%d", &height);
+    printf("Enter length of box: ");
+}
