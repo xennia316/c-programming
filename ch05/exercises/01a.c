94f4335bdb199f9c237c08dabc6f16e13d234cb5
xennia316 <xennia316@gmail.com>
2021-11-17 21:12:28 +0100

    simple exercises under chapter one

---
+++ ch05/exercises/01a.c
@@ -0,0 +1,25 @@
+/**
+ * @file 01a.c
+ * @author xennia316
+ * @brief finding the value of k 
+ * @version 0.1
+ * @date 2021-11-17
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    int i, j, k;
+
+    i = 2; 
+    j = 3;
+
+    k = i * j == 6;
+
+    
+    printf("%d\n", k);
+    return 0;
+}
---
+++ ch05/exercises/01b.c
@@ -0,0 +1,22 @@
+/**
+ * @file 01b.c
+ * @author xennia316
+ * @brief complex printing
+ * @version 0.1
+ * @date 2021-11-17
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    int i, j, k;
+
+    i = 5;
+    j = 10;
+    k = 1;
+        printf("%d\n", k > i < j);
+        return 0;
+}
