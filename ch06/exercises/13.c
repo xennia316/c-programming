d30da169e53355c1499a635bbe963e7f0c59c97c
xennia316 <xennia316@gmail.com>
2021-12-06 22:05:38 +0100

    rewriting a loop

---
+++ ch06/exercises/13.c
@@ -0,0 +1,27 @@
+/**
+ * @file 13.c
+ * @author xennia316
+ * @brief Rewriting a given program.
+ * @version 0.1
+ * @date 2021-12-06
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    
+    int n;
+    int m ;
+
+    for(n = 0; m /= 2 && m > 0; n++)
+        {
+            // writing a printf statement to make output  vissible.
+            // printf("\n");
+            // printf("Ready\n");
+            
+        }
+        return 0;
+}
