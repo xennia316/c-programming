070b6848b087c980887ae818e259dd5ee218a66c
xennia316 <xennia316@gmail.com>
2021-12-06 22:11:40 +0100

    debugging if statement

---
+++ ch06/exercises/14.c
@@ -0,0 +1,21 @@
+/**
+ * @file 14.c
+ * @author xennia316
+ * @brief correcting a for loop
+ * @version 0.1
+ * @date 2021-12-06
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    int n ;
+
+    if(n % 2 == 0)
+    printf("n is even \n");
+
+    return 0;
+}
