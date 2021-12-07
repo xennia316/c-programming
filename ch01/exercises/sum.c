2a1cfcc500bcd2d9e10b79be87e9fcc64a378c8e
xennia316 <xennia316@gmail.com>
2021-12-07 23:58:16 +0100

    summing two numbers

---
+++ ch01/exercises/sum.c
@@ -0,0 +1,32 @@
+/**
+ * @file sum.c
+ * @author xennia316
+ * @brief sum of 2 numbers
+ * @version 0.1
+ * @date 2021-12-07
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    int a;
+    int b;
+    int c;
+
+    printf("Please enter a number");
+    scanf("%d", &a);
+
+    printf(" Enter another number");
+    scanf("%d", &b);
+
+    c = a + b;
+
+    printf("Sum = %d", c);
+
+    return 0;
+
+
+}
