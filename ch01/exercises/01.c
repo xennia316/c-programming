4d5c0d441190001c1fec9a8b10e66deee8f94647
xennia316 <xennia316@gmail.com>
2021-12-07 23:53:36 +0100

    print Hello world

---
+++ ch01/exercises/01.c
@@ -0,0 +1,18 @@
+/**
+ * @file 01.c
+ * @author xennia316
+ * @brief hello world
+ * @version 0.1
+ * @date 2021-12-07
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello world\n");
+
+    return 0;
+}
