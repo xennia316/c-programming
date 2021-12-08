f02de1a418c058a3f56249237b6687645775846e
xennia316 <xennia316@gmail.com>
2021-12-08 22:01:23 +0100

    displaying with a for loop

---
+++ abstracts/display.c
@@ -0,0 +1,29 @@
+/**
+ * @file display.c
+ * @author xennia316
+ * @brief Displaying ten numbers
+ * @version 0.1
+ * @date 2021-12-08
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <stdio.h>
+
+int main(void)
+{
+
+    int a;
+    int i;
+
+    printf("Numbers from 0 to 09: \n");
+
+    for(i = 0; i <= 9; i++)
+    
+    
+        printf("%d\n", i);
+
+        return 0;
+    
+
+}
