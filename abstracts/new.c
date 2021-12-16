6f6135324a6f525f529592e3b1f5b6900c1f845f
xennia316 <xennia316@gmail.com>
2021-12-16 21:04:27 +0100

    a

---
+++ abstracts/new.c
@@ -0,0 +1,19 @@
+/**
+ * @file new.c
+ * @author xennia316
+ * @brief 
+ * @version 0.1
+ * @date 2021-12-16
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    int a;
+    printf("%d", a*5);
+
+    return 0;
+}
