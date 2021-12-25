b7a808213b5235dd6cecc860d6d051f9f7caeff6
xennia316 <xennia316@gmail.com>
2021-12-25 04:12:27 +0100

    A polynomial function with mathematical operators

---
+++ ch02/projects/05.c
@@ -0,0 +1,25 @@
+/**
+ * @file 05.c
+ * @author Sonia Amehmbo @xennia316
+ * @brief Polynomial 
+ * @version 0.1
+ * @date 2021-12-25
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <stdio.h>
+int main(void)
+{
+    int x;
+    int function;
+
+    printf("Enter a value for x: ");
+    scanf("%d", &x);
+
+    function = 3 * x * x * x * x * x + 2 * x * x * x * x + 5 * x * x * x - x * x + 7 * x - 6;
+
+    printf("The answer is: %d\n", function);
+
+    return 0;
+}
