d0358a4c430f1a9f1d95eeea2a9c819b3eef0b5b
xennia316 <xennia316@gmail.com>
2021-12-25 03:50:31 +0100

    Displaying a tick

---
+++ ch02/projects/01.c
@@ -0,0 +1,28 @@
+/**
+ * @file 01.c
+ * @author Sonia Amehmbo@xennia316
+ * @brief Displaying a tick
+ * @version 0.1
+ * @date 2021-12-25
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <stdio.h>
+int main(void)
+{
+    printf("                               *\n");
+    printf("                             *\n");
+    printf("                           *\n");
+    printf("                         *\n");
+    printf("                       *\n");
+    printf("                     *\n");
+    printf("         *         *\n");
+    printf("          *      *\n");
+    printf("           *   *\n");
+    printf("            *\n");
+
+    return 0;
+
+
+}
