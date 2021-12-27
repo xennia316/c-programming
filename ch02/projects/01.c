69483283567962dcf319357cfdca8eb342f8f112
xennia316 <xennia316@gmail.com>
2021-12-27 18:38:29 +0100

    space arrangements

--- ch02/projects/01.c
+++ ch02/projects/01.c
@@ -22,7 +22,7 @@ int main(void)
     printf("           *   *\n");
     printf("            *\n");
 
-    return 0;
+    return 0;                                                                                                       
 
 
 }
