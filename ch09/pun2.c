e56cd71bd72a10443584579584d7a7d33b16fbd8
xennia316 <xennia316@gmail.com>
2022-01-07 03:51:52 +0100

    using void parameter functions

---
+++ ch09/pun2.c
@@ -0,0 +1,22 @@
+/**
+ * @file pun2.c
+ * @author Sonia Amehmbo @xennia316
+ * @brief Prints a bad pun
+ * @version 0.1
+ * @date 2022-01-07
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <stdio.h>
+
+void print_pun(void)
+{
+    printf("To C or not to C, that is the question.\n");
+}
+
+int main(void)
+{
+    print_pun();
+    return 0;
+}
