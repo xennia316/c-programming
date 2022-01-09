e45de264b1e23f31eb2371099722e1bb39e854dd
xennia316 <xennia316@gmail.com>
2022-01-09 17:54:21 +0100

    prototypical internet newbie

---
+++ ch08/projects/06.c
@@ -0,0 +1,20 @@
+/**
+ * @file 06.c
+ * @author @xennia316
+ * @brief prototypical Internet newbie
+ * @version 0.1
+ * @date 2022-01-09
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+    char str[30];
+
+
+}
