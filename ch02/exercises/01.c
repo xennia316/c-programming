af4d5d1856af111e4ef265764a3467bd1654ccfc
xennia316 <xennia316@gmail.com>
2022-01-15 17:53:54 +0100

    Kernighan and Ritchie's famous program

---
+++ ch02/exercises/01.c
@@ -0,0 +1,16 @@
+/**
+ * @file 01.c
+ * @author Sonia Amehmbo @xennia316
+ * @brief Kernighan and Ritchie's famous program
+ * @version 0.1
+ * @date 2022-01-15
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello world!\n");
+}
