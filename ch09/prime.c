650e273ca6393bc8c62935f42b7f0fc09825d95b
xennia316 <xennia316@gmail.com>
2022-02-05 20:23:50 +0100

    Added comments

--- ch09/prime.c
+++ ch09/prime.c
@@ -11,7 +11,8 @@
 
 #include <stdio.h>
 #include <stdbool.h>
-
+// function for finding the prime numbers
+//bool returns true or false only
 bool is_prime(int n)
 {
     int divisor;
@@ -27,7 +28,7 @@ bool is_prime(int n)
 int main(void)
 {
     int n;
-    
+    // after calling is_prime with parameter n returns a true value then "Print" else print "Not prime"
     printf("Enter a number: ");
     scanf("%d", &n);
     if (is_prime(n))
