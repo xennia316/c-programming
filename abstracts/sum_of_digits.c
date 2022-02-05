b66f02c4eba2d148f10143bda21465491354eb05
xennia316 <xennia316@gmail.com>
2022-02-05 20:48:57 +0100

    Added comments

--- abstracts/sum_of_digits.c
+++ abstracts/sum_of_digits.c
@@ -10,20 +10,25 @@
  */
 #include <stdio.h>
 
+//Addition is commutative and accessing digits from behind is relatively easier.
+//The addition is done from the last digit
 int main(void)
 {
+    // sum initialised to zero
     int number, digit, sum = 0;
 
     printf("Enter a number: ");
     scanf("%d", &number);
 
-    while ( number != 0)
+    // while the number entered is not zero perform addition on all digits entered
+    while (number != 0)
     {
         digit = number % 10;
         sum = sum + digit;
+        // the number is divided by ten everytime to eliminate the digit already added
         number = number / 10;
     }
-    
+
     printf("The sum of digits of %d is %d\n", number, sum);
 
     return 0;
