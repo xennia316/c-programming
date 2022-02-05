0eb9ab662522353d431810f5af646468cbee9b6d
xennia316 <xennia316@gmail.com>
2022-02-05 20:29:14 +0100

    comments added

--- abstracts/lcm.c
+++ abstracts/lcm.c
@@ -17,17 +17,19 @@ int main(void)
     printf("Enter two numbers: ");
     scanf("%d %d", &num1, &num2);
     printf("\n\n");
-
+    // if num1 is bigger than num2 then num1 is assigned to max else num2 is assigned to max
     max = (num1 > num2) ? num1 : num2;
 
     while(1)
     {
+        //if there's no remainder when max is divided by num1 and num2 then max is the lcm
         if(max % num1 == 0 && max % num2 == 0)
         {
             printf("%d is the lcm of %d and %d", max, num1, num2);
             printf("\n\n");
             break;
         } 
+        //Use max then increment it
          ++max;
     }
     return 0;
