4aaa689e696875574630c6e4019e9d53ff7a1404
xennia316 <xennia316@gmail.com>
2022-02-08 19:27:39 +0100

    count1.c

---
+++ abstracts/count1.c
@@ -0,0 +1,21 @@
+/**
+ * @file count1.c
+ * @author @xennia316
+ * @brief counts from 1 to 10
+ * @version 0.1
+ * @date 2022-02-08
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    int i, j;
+    for (i = 1; i <= 10; i++)
+        printf("%3d", i);
+    printf("\n");
+
+    return 0;
+}
