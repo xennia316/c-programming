11274acdf3a6d7e389f795b3cf71f0e9b5bfc1a4
xennia316 <xennia316@gmail.com>
2022-02-08 19:42:08 +0100

    Increased functionalities

--- abstracts/count1.c
+++ abstracts/count1.c
@@ -1,7 +1,7 @@
 /**
  * @file count1.c
  * @author @xennia316
- * @brief counts from 1 to 10
+ * @brief counts from 1 to 10 for a specific number of times
  * @version 0.1
  * @date 2022-02-08
  * 
@@ -10,12 +10,29 @@
  */
 #include <stdio.h>
 
-int main(void)
+//PLEASE FIX ME!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!.
+int first(void)
 {
-    int i, j;
+    int i;
     for (i = 10; i >= 1; i++)
         printf("%3d", i);
     printf("\n");
 
+    for (i = 1; i <= 10; i++)
+        printf("%3d", i);
+    printf("\n");
+    return 0;
+}
+
+int main(void)
+{
+    int a, j;
+    printf("Enter a  value: ");
+    scanf("%d", &a);
+
+    for(j = 0; j <= a; j++)
+    {
+        first();
+    }
     return 0;
 }
